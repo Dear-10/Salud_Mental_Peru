ab0eb4ea586fe311326ceff0ae9f2c2adc55bf4b
Andre <deymaaar@gmail.com>
2025-11-28 15:35:55 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Micasita } from "./screens/Micasita";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
-  <Micasita />
+  <StrictMode>
+    <Micasita />
+  </StrictMode>,
 );
